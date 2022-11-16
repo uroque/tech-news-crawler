877d85f9a0a47cd29c38d29aaed52260a3f90a81
Ulisses Roque <ulisses.roque@gmail.com>
2022-11-15 22:09:52 -0300

    feat: implements scrape_noticia function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -33,7 +33,19 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return {
+        "url": selector.css("[rel='canonical']::attr(href)").get(),
+        "title": selector.css(".entry-title::text").get().strip(),
+        "timestamp": selector.css(".meta-date::text").get(),
+        "writer": selector.css(".author a.url::text").get(),
+        "category": selector.css(".category-style span.label::text").get(),
+        "comments_count": len(selector.css(".comment-list li").getall()),
+        "tags": selector.css("a[rel='tag']::text").getall(),
+        "summary": "".join(
+            selector.css(".entry-content > p:nth-of-type(1) ::text").getall()
+        ).strip(),
+    }
 
 
 # Requisito 5
