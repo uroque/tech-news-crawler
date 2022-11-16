6aafaf454d0ff68af6203dafede966a8357eb9fd
Ulisses Roque <ulisses.roque@gmail.com>
2022-11-16 10:16:57 -0300

    feat: implements seach_by_category function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -28,4 +28,5 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    query = {"category": {"$regex": category, "$options": "i"}}
+    return [(news['title'], news['url']) for news in search_news(query)]
