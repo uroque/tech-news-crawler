7b08651bcc4075c47b0e7c2f79c753516b7677ec
Ulisses Roque <ulisses.roque@gmail.com>
2022-11-15 21:47:36 -0300

    feat: implements scrape_next_page function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -27,7 +27,8 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    css_to_parse = "a.next::attr(href)"
+    return Selector(html_content).css(css_to_parse).get()
 
 
 # Requisito 4
